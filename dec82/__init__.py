c27eabe3794f3d5d5a081ce024f0c77771b0a237
kamangir <arash@kamangir.net>
2022-11-17 17:59:13 -0800

    prep for outsource - kamangir/bolt#731

--- dec82/__init__.py
+++ dec82/__init__.py
@@ -1,5 +1,5 @@
 NAME = "dec82"
 
-VERSION = "3.12.1"
+VERSION = "3.14.1"
 
 from .functions import *
--- dec82/functions.py
+++ dec82/functions.py
@@ -7,17 +7,5 @@ logger = logging.getLogger(__name__)
 
 
 def validate_hardware():
-    from grove.grove_ryb_led_button import GroveLedButton
-
-    ledbtn = GroveLedButton(5)
-
-    while True:
-        ledbtn.led.light(True)
-        time.sleep(1)
-
-        ledbtn.led.light(False)
-        time.sleep(1)
-
-        print("----")
-
+    ...
     return True
