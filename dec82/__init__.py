547a52f541c718ff879b7e0c14cc4becc0ff98b8
kamangir <arash@kamangir.net>
2022-12-02 21:39:47 -0800

    dec82 interface design - kamangir/bolt#731

--- dec82/__init__.py
+++ dec82/__init__.py
@@ -1,6 +1,6 @@
 NAME = "dec82"
 
-VERSION = "4.20.1"
+VERSION = "4.22.1"
 
 from .classes import *
 from .functions import *
--- dec82/classes.py
+++ dec82/classes.py
@@ -25,9 +25,7 @@ class Dec82(object):
         self.line_count = 8
         self.line_length = 21
 
-        self.hr = self.line_length * ["-"]
-
-        self.log = []
+        self.history = []
 
         # https://github.com/IcingTomato/Seeed_Python_SSD1315/blob/master/examples/stats.py
         self.display = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
@@ -56,34 +54,25 @@ class Dec82(object):
 
         self.font = ImageFont.load_default()
 
+    def log(line):
+        self.history = self.line_count([""]) + ["", line, ""]
+
     def process_image(self, frame, image):
-        self.log += [
-            self.hr,
-            "",
-            f"frame #{frame}: {string.pretty_shape_of_matrix(image)}",
-            "",
-            self.hr,
-        ]
+        self.log(f"frame #{frame}: {string.pretty_shape_of_matrix(image)}")
 
     def process_message(self, messages):
-        self.log += [
-            self.hr,
-            "",
-            message.process_message(),
-            "",
-            self.hr,
-        ]
+        self.log(message.process_message())
         return None
 
     def step(self, session):
         ...
 
     def update_screen(self, session):
-        if len(self.log) < self.line_count:
-            self.log += (" | ".join(session.signature())).split(" | ")
+        if len(self.history) < self.line_count:
+            self.history += (" | ".join(session.signature())).split(" | ")
 
         if self.timer.tick("wait"):
-            self.log = self.log[1:]
+            self.history = self.history[1:]
 
         self.draw.rectangle(
             (0, 0, self.display.width, self.display.height),
