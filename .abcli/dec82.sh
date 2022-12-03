860a819d409d66552d8d6e20157f79296db8c741
kamangir <arash@kamangir.net>
2022-12-02 20:22:42 -0800

    dec82 interface design - kamangir/bolt#731

--- .abcli/dec82.sh
+++ .abcli/dec82.sh
@@ -20,25 +20,23 @@ function dec82() {
         return
     fi
 
-    if [ "$task" == "start" ] ; then
-        abcli_log "dec82: session started."
-
-        abcli_tag set \
-            $abcli_object_name \
-            open,$abcli_hostname,dec82
-
-        local options="$2"
-
-        python3 -m dec82 \
-            start_session \
-            ${@:3}
-
-        abcli_upload open
-
-        abcli_log "dec82: session ended."
-
-        return
-    fi
-
     abcli_log_error "-dec82: $task: command not found."
+}
+
+function dec82_session() {
+    abcli_log "dec82: session started."
+
+    abcli_tag set \
+        $abcli_object_name \
+        open,$abcli_hostname,dec82
+
+    local options=$1
+
+    python3 -m dec82 \
+        start_session \
+        ${@:2}
+
+    abcli_upload open
+
+    abcli_log "dec82: session ended."
 }
